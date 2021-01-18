5b1692f667d289b23e87f3e4efb8a41e6630dcbd
Maico Kley <maicokley@hotmail.com>
2021-01-17 22:27:10 -0300

    parte 4

--- redux/src/pages/Home/index.js
+++ redux/src/pages/Home/index.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
 import { MdAddShoppingCart } from 'react-icons/md';
 import { formatPrice } from '../../util/format';
 import api from '../../services/api';
 
+import * as CartActions from '../../store/modules/cart/actions';
+
 import { ProductList } from './styles';
 
-export default class Home extends Component{
+class Home extends Component{
   state = {
     products: []
   };
@@ -21,8 +25,15 @@ export default class Home extends Component{
     this.setState({ products: data });
   }
 
+  handleAddProduct = id => {
+    const { addToCartRequest } = this.props;
+
+    addToCartRequest(id);
+  };
+
   render() {
     const { products } = this.state;
+    const { amount } = this.props;
 
     return (
       <ProductList>
@@ -35,9 +46,13 @@ export default class Home extends Component{
           <strong>{product.title} </strong>
           <span>{product.priceFormatted}</span>
 
-          <button type="button">
+          <button 
+            type="button" 
+            onClick={() => this.handleAddProduct(product.id)}
+          >
             <div>
-              <MdAddShoppingCart size={16} color="#fff" /> 3
+              <MdAddShoppingCart size={16} color="#fff" /> 
+              {amount[product.id] || 0}
             </div>
 
             <span>ADICIONAR AO CARRINHO</span>
@@ -48,3 +63,19 @@ export default class Home extends Component{
     );
   }
 }
+
+const mapStateToProps = state => ({
+  amount: state.cart.reduce((amount, product) => {
+    amount[product.id] = product.amount;
+
+    return amount;
+  }, {})
+});
+
+const mapDispatchToProps = dispatch => 
+  bindActionCreators(CartActions, dispatch);
+
+export default connect(
+  mapStateToProps, 
+  mapDispatchToProps
+)(Home);
